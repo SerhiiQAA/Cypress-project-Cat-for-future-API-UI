9f4b1c1a275ccbb811142053c0d80e169715b7e3
SerhiiQA <nabukve@gmail.com>
2024-02-12 20:58:52 +0200

    1

---
+++ cypress/e2e/happyStories.cy.js
@@ -0,0 +1,19 @@
+describe('HappyStories', () => {
+    it('HappyStories', () => {
+      cy.visit('https://cat-for-future.crabdance.com/')
+      cy.viewport(1920,1080)
+      cy.location('protocol').should('eq','https:')
+
+      cy.get('._container_ni4e4_12 > ._menu_15spm_33 > ._list_15spm_1 > :nth-child(2) > a')
+      .should('be.visible')
+      .contains('Щасливі історії')
+      .click()
+
+      cy.get(':nth-child(2) > ._cardBody_5718g_45 > ._btnWrap_5718g_101 > ._btn_5718g_101')
+      .contains('Читати далі')
+      .click()
+    
+      cy.contains('Згорнути')
+      .click()  
+    })
+  })
