8738f42ff47429c192d0e89a005c3451ae863706
SerhiiQA <nabukve@gmail.com>
2024-02-12 21:23:40 +0200

    2

---
+++ cypress/e2e/ourCats.cy.js
@@ -0,0 +1,37 @@
+describe('Our Cats', () => {
+    it('Our Cats', () => {
+      cy.visit('https://cat-for-future.crabdance.com/')
+      cy.viewport(1920,1080)
+      cy.location('protocol').should('eq','https:')
+
+      cy.get('._container_ni4e4_12 > ._menu_15spm_33 > ._list_15spm_1 > :nth-child(1) > a')
+      .should('be.visible')
+      .contains('Перегляд кошенят')
+      .click()
+
+      cy.get('div._wrapper_u3kbr_1:nth-child(1) > div:nth-child(1) > div:nth-child(1) > img:nth-child(1)')
+      .click()
+
+      cy.get('._wrapper_1ipm8_20 > ._wrapper_u3kbr_1')
+      .should('be.visible')
+      .contains('Забронювати')
+      cy.get('._selectedImage_1ar4d_1 > img')
+      .should('be.visible')
+
+      cy.get('._imagesRow_1ar4d_14 > :nth-child(2) > img')
+      .should('be.visible')
+      .click()  
+
+      cy.get('._imagesRow_1ar4d_14 > :nth-child(3) > img')
+      .should('be.visible')
+      .click() 
+
+      cy.get('._imagesRow_1ar4d_14 > :nth-child(4) > img')
+      .should('be.visible')
+      .click() 
+
+      cy.get('._closeIcon_1ipm8_26')
+      .should('be.visible')
+      .click() 
+    })
+  })
