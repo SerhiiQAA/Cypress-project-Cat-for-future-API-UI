3e6bc435c9ab3c702deaff4364f4d7600511c27b
SerhiiQA <nabukve@gmail.com>
2024-02-15 12:14:57 +0200

    1

--- cypress/e2e/cookies.cy.js
+++ cypress/e2e/cookies.cy.js
@@ -2,36 +2,27 @@ describe('Cookies', () => {
   it('cookies', () => {
     cy.viewport(1920,1080)
     cy.visit('https://cat-for-future.crabdance.com/')
-    cy.get('._container_ni4e4_12 > ._menu_15spm_33 > ._list_15spm_1 > :nth-child(1) > a')
-    .should('be.visible')
-    .contains('Перегляд кошенят')
-    .click()
-    cy.url().should('include','ourCats')
 
-    cy.get('._container_ni4e4_12 > ._menu_15spm_33 > ._list_15spm_1 > :nth-child(2) > a')
+// cookies
+    cy.get('._cookie_1sbl7_1')
     .should('be.visible')
-    .contains('Щасливі історії')
-    .click()
-    cy.url().should('include','happyStories')
 
-    cy.get('._container_ni4e4_12 > ._menu_15spm_33 > ._list_15spm_1 > :nth-child(3) > a')
+// cookies buttons
+    cy.get('._link_1sbl7_67')
+    .contains('Політика конфіденційності')
     .should('be.visible')
-    .contains('Контакти')
-    .click()
-    cy.url().should('include','contacts')
 
-    cy.get('._btnWrapper_ni4e4_35 > ._primaryBtn_a4x38_11')
+    cy.get('._cookie_1sbl7_1 > ._button_a4x38_2')
+    .contains('OK')
     .should('be.visible')
-    .contains('Допомогти')
-    .click()
-    cy.get('._modalWrapper_1j588_38').should('be.visible')
-    cy.get('._close_1j588_90').click()
 
-    cy.get('._container_ni4e4_12 > ._logo_tsyqa_1 > a > img')
+// close cookies
+    cy.get('._close_1sbl7_39')
     .should('be.visible')
     .click()
-    cy.get('._contentBtn_1gw6e_178 > ._button_a4x38_2')
-    .contains('Знайти друга')
-    .should('be.visible')
+
+// checking the closure of cookies
+    cy.get('._cookie_1sbl7_1')
+    .should('not.be.visible')
   })
 })
