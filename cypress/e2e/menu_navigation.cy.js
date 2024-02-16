7b27bda639a5e1adf2bad6dd956d369a5d3301b5
SerhiiQA <nabukve@gmail.com>
2024-02-16 15:56:36 +0200

    1

--- cypress/e2e/menu_navigation.cy.js
+++ cypress/e2e/menu_navigation.cy.js
@@ -2,13 +2,9 @@ describe('Menu', () => {
     it('Navigation menu', () => {
       cy.visit('https://cat-for-future.crabdance.com/')
       cy.viewport(1920,1080)
-      cy.location('protocol').should('eq','https')
+      cy.location('protocol').should('eq','https:')
       cy.get('._contentBtn_1gw6e_178 > ._button_a4x38_2').should('be.visible')
       cy.get('._cookie_1sbl7_1 > ._button_a4x38_2').click() 
-    //   cy.get('div[class="_cookie_1sbl7_1"]').should('be.visible')
-    //   cy.get('#root > div > div._cookie_1sbl7_1 > div._text_1sbl7_50 > a').should('have.text','Політика конфіденційності')
-    //   cy.get('._cookie_1sbl7_1 > ._button_a4x38_2').should('be.visible')
-    //   cy.get('._close_1sbl7_39').should('be.visible')
 
     cy.get('._container_ni4e4_12 > ._menu_15spm_33 > ._list_15spm_1 > :nth-child(1) > a')
     .should('be.visible')
